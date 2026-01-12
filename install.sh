d50a8a5c1a81bf4cfc06feed70d288324073512e
Milkiway13 <160399313+Milkiway13@users.noreply.github.com>
2026-01-12 10:36:14 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo """NOTE: it's reccomended to change repos in /etc/pacman.d/mirrorlist to Server = https://archive.archlinux.org/repos/2025/09/25/$ repo/os/$ arch (remove spaces after $), so everything will work properly, because new versions of hyprland break windowrules"""
+echo "also you need to manually change qt and gtk themes in gnome-tweaks and qt5/6 settings"
+sudo pacman -S nemo alacritty bc swaync waybar hyprlock ranger zathura bat fuzzel translate-shell gnome-tweaks
+paru -S bzmenu
+cp -r ./.config/* ~/.config/
+chsh -s /bin/fish
+cp -r ./wallpapers ~/Pictures/
