7292215a2f134652372a018235fcb7264e1a3570
Milkiway13 <160399313+Milkiway13@users.noreply.github.com>
2026-01-12 10:40:58 +0300

    changed notes
    Updated installation instructions for theme changes.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo """NOTE: it's reccomended to change repos in /etc/pacman.d/mirrorlist to Server = https://archive.archlinux.org/repos/2025/09/25/$ repo/os/$ arch (remove spaces after $), so everything will work properly, because new versions of hyprland break windowrules"""
-echo "also you need to manually change qt and gtk themes in gnome-tweaks and qt5/6 settings"
+echo "also you need to manually change qt theme in qt5/6 settings"
 sudo pacman -S nemo alacritty bc swaync waybar hyprlock ranger zathura bat fuzzel translate-shell gnome-tweaks
 paru -S bzmenu
 cp -r ./config/* ~/.config/
