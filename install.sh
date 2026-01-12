20c4bf83d47a7c5bac9f17bd313378aad87f3a51
Milkiway13 <160399313+Milkiway13@users.noreply.github.com>
2026-01-12 10:48:48 +0300

    added neovim, removed gnome-tweaks
    Updated package installation in install.sh to include neovim.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo """NOTE: it's reccomended to change repos in /etc/pacman.d/mirrorlist to Server = https://archive.archlinux.org/repos/2025/09/25/$ repo/os/$ arch (remove spaces after $), so everything will work properly, because new versions of hyprland break windowrules"""
 echo "also you need to manually change qt theme in qt5/6 settings"
-sudo pacman -S nemo alacritty bc swaync waybar hyprlock ranger zathura bat fuzzel translate-shell gnome-tweaks
+sudo pacman -S nemo alacritty bc swaync waybar hyprlock ranger zathura bat fuzzel translate-shell neovim
 paru -S bzmenu
 cp -r ./config/* ~/.config/
 chsh -s /bin/fish
