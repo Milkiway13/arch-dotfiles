af798075d5d3476b1733dc8c19cf8922bc4d5f73
Milkiway13 <160399313+Milkiway13@users.noreply.github.com>
2026-01-17 16:45:44 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 echo """NOTE: it's reccomended to change repos in /etc/pacman.d/mirrorlist to Server = https://archive.archlinux.org/repos/2025/09/25/$ repo/os/$ arch (remove spaces after $), so everything will work properly, because new versions of hyprland break windowrules"""
 echo "also you need to manually change qt theme in qt5/6 settings"
-sudo pacman -S nemo alacritty bc swaync waybar hyprlock ranger zathura bat fuzzel translate-shell neovim
+sudo pacman -S hyprland nemo alacritty bc swaync waybar hyprlock ranger zathura bat fuzzel translate-shell gnome-tweaks
 paru -S bzmenu
-cp -r ./config/* ~/.config/
+cp -rT ./config/. ~/.config/
+mkdir -p ~/.config/milki_dots/
+cp -r ./themes/. ~/.config/milki_dots/
 chsh -s /bin/fish
 cp -r ./wallpapers ~/Pictures/
+mkdir -p ~/.config/milki_dots/
+cp -r ./themes/. ~/.config/milki_dots/
+cp -r ./theme-changers ~/.local/bin/
+cp -r ./gtk-themes/. ~/.themes/
+gsettings set org.gnome.desktop.interface gtk-theme "Gruvbox-Dark"
